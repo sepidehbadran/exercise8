aa9ed4256cd28a24fbd46dc5e7f14ef5e4f3cf48
Sepideh <sepideh.badran05@gmail.com>
2024-03-19 20:04:48 +0330

    exersice8

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+static  void main ( string[] args )
+{
+    List <int> num2 = new List<int> { 1,2,4,8,16,64};
+    List <int> num1 = new List<int> { 1,2,4,8,16,64};
+   num2.Reverse();
+    for (int i = 0; i < num2.Count; i++)
+    {
+        Console.WriteLine(Convert.ToString(num1[i])) ;
+
+
+    }
+}
+        
